6289deb07497295d10090119bc823ee6170a695c
Ricky Peters <ericrickyallenpeters@gmail.com>
2017-11-15 11:33:24 -1000

    initial commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+const counter = {
+  count: 0
+}
+
+app.get('/api/counter', (req, res) => {
+  res.json(counter)
+})
+app.get('/api/counter/increment', (req, res) => {
+  ++counter.count;
+  res.end();
+})
+app.get('/api/counter/decrement', (req, res) => {
+  --counter.count;
+  res.end();
+})
+
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`);
+});
